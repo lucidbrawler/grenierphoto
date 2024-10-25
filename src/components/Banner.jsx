522806ec6177c586847a6527341baeb56222a329
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-10-25 15:02:12 -0400

    image normalized on banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import GoodFox from '../lowrezimages/goodfox.jpeg';
-import GreatEagle from '../lowrezimages/greateagle.jpg';
-import GodsCanvas from '../lowrezimages/GodsCanvas.jpg';
-import Seascapes from '../lowrezimages/seascape.jpeg';
-import Greatcityshot from '../lowrezimages/greatcityshot.jpg';
-import Sunprint from '../lowrezimages/sunprint.jpg';
+import GoodFox from '../jodyimages/goodfox.jpg';
+import GreatEagle from '../jodyimages/greateagle.jpg';
+import GodsCanvas from '../jodyimages/GodsCanvas.jpg';
+import Seascapes from '../jodyimages/seascape.jpeg';
+import Greatcityshot from '../jodyimages/greatcityshotcopy.jpg';
+import Sunprint from '../jodyimages/sunprint.jpg';
 const Banner = () => {
   return (
     <div>
