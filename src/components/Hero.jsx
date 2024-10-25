4c20b3ff9c4eab0ad96f2844ae33e2290906202f
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-10-25 17:51:19 -0400

    color about me and padding after trigger

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const iconarrow =  <LuArrowDownSquare />;
          <img className='portrait ' src={headshot} alt=''/>
          <h3></h3>
         
-<Collapsible trigger={<div>About me <LuArrowDownSquare /></div>}>
+<Collapsible trigger={<div className='aboutme'>About me <LuArrowDownSquare /></div>}>
          <p className='portraitBio'> I have been an artist my entire life. I was driven to teach myself to play piano at an early age 
             and become a professional pianist. This was a true gift. I’ve painted, written songs, and have made some of the best pizza around the world. 
 
