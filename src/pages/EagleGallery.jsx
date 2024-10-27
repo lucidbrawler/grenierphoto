53e2d0b2498e09004a862bda6ca5e9c14d48fadf
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-10-26 20:36:07 -0400

    used combo gallery for man page and cleared extra from eagles gallery

--- src/pages/EagleGallery.jsx
+++ src/pages/EagleGallery.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 
 import {Nav } from 'react-bootstrap';
-import Cuse1 from '../jodyimages/syracuse/clinton1.jpeg';
-import Cuse2 from '../jodyimages/syracuse/clinton2.jpg';
-import Cuse3 from '../jodyimages/syracuse/reddress2.jpg';
-import Cuse4 from '../jodyimages/syracuse/syracuse1.jpeg';
-import Cuse5 from '../jodyimages/syracuse/christmasGold.jpg';
-import Cuse6 from '../jodyimages/syracuse/greatcityshotcopy.jpg';
+
 import GreatEagle from '../jodyimages/eagles/eagle1.jpeg';
 import Eagle2 from '../jodyimages/eagles/eaglefish3.jpeg';
 import Eagle3 from '../jodyimages/eagles/eagle2.jpeg';
@@ -14,10 +9,7 @@ import Eagle4 from '../jodyimages/eagles/eaglesfish2.jpeg';
 import Eagle5 from '../jodyimages/eagles/eaglefish2.jpeg';
 import Eagle6 from '../jodyimages/eagles/greateagle.jpg';
 import Eagle7 from '../jodyimages/eagles/eaglewithfish.jpg';
-import Wildlife1 from '../jodyimages/wildlife/goodfox.jpg';
-import Wildlife2 from '../jodyimages/wildlife/foxtounge.jpeg';
-import Seascape1 from '../jodyimages/seascapes/GodsCanvas.jpg';
-import Seascape2 from '../jodyimages/seascapes/seascape.jpeg';
+
 const EagleGallery = () => {
   return (
     <div className='Grid-wrapper'>
@@ -28,31 +20,20 @@ const EagleGallery = () => {
         <Nav.Link href='https://buy.stripe.com/dR6dTrdDraohaQgaEI'>
         <img className='' src={GreatEagle}/>
         </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Cuse4}/>
-        </Nav.Link>
-        
+     
         <Nav.Link>
         <img className='' src={Eagle7}/>
         </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Seascape1}/>
-        </Nav.Link>
-      
+     
         <Nav.Link>
         <img className='' src={Eagle2}/>
         </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Wildlife1}/>
-        </Nav.Link>
-      
+     
         
         <Nav.Link>
         <img className='' src={Eagle3}/>
         </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Cuse2}/>
-        </Nav.Link>
+       
         <Nav.Link>
         <img className='' src={Eagle4}/>
         </Nav.Link>
@@ -62,22 +43,13 @@ const EagleGallery = () => {
       
       
     
-        <Nav.Link>
-        <img className='' src={Cuse3}/>
-        </Nav.Link>
-    
-        <Nav.Link>
-        <img className='' src={Cuse5}/>
-        </Nav.Link>
+   
+       
         <Nav.Link>
         <img className='' src={Eagle5}/>
         </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Cuse6}/>
-        </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Wildlife2}/>
-        </Nav.Link>
+      
+     
        
       
         <Nav.Link>
@@ -85,12 +57,7 @@ const EagleGallery = () => {
         </Nav.Link>
         
       
-        <Nav.Link>
-        <img className='' src={Seascape2}/>
-        </Nav.Link>
-        <Nav.Link>
-        <img className='' src={Cuse1}/>
-        </Nav.Link>
+  
        
     </div>
     </div>
