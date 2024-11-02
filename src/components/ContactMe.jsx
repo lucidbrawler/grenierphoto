4afb882366ac1674309b9a313df4fc1dd0651912
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-11-02 16:00:52 -0400

    form on bottom

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const ContactMe = () => {
   return (
-    <div className='ContactMe'>Contact Me</div>
+    <div className='ContactMe'>
+      <form name="contact" method="POST" data-netlify="true">
+  <input type="hidden" name="subject" 
+  value="Sales inquiry from mysitename.netlify.app" />
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+    </div>
   )
 }
 
