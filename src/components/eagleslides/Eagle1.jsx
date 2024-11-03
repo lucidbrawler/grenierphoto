985b84cc63327a54b6c84ca9d10daaf0e637dd19
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-11-03 16:13:03 -0500

    working buy link for metal 18x58

--- src/components/eagleslides/Eagle1.jsx
+++ src/components/eagleslides/Eagle1.jsx
@@ -1,17 +1,19 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import {Nav } from 'react-bootstrap';
 
 import Eagle1 from '../../jodyimages/eagles/eagle1.jpeg';
 
 const Eagleone = () => {
+
+  const [buylink, setBuylink] = useState('')
   return (
     <div>
     <div className='archives'>
     <a href='/Eagle4'>
         <button className='button1'>prev</button>
         </a>
-    <Nav.Link className='' href='https://buy.stripe.com/dR6dTrdDraohaQgaEI'>
+    <Nav.Link className='' >
    
     <img className='archives1' src={Eagle1}/>
    
@@ -23,14 +25,14 @@ const Eagleone = () => {
     <section className='printOptions'>
     
     <select className='button' onChange={(event) => {
-        setMeasurement1(event.target.value);
+        setBuylink(event.target.value);
       }}>
          <option value="Cups"> paper: 8x16</option>
         <option value="Cup">paper: 12x36</option>
         <option value="fluid oz">paper: 18x58</option>
         <option value="Cups">metal: 8x16</option>
         <option value="Cup">metal: 12x36</option>
-        <option value="fluid oz">metal: 18x58</option>
+        <option value="https://buy.stripe.com/dR6dTrdDraohaQgaEI">metal: 18x58</option>
         <option value="Cups"> framed: 8x16</option>
         <option value="Cup">framed: 12x36</option>
         <option value="fluid oz">framed: 18x58</option>
@@ -39,7 +41,9 @@ const Eagleone = () => {
  
       </section>
       <section className='archives'>
+        <a href={buylink}>
       <button className='buybutton'>Go to Checkout</button>
+      </a>
       </section>
 </div>
   )
