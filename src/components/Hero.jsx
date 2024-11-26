508758c3c32149157bc9163fe4573a8e26fa737c
jax <kennethpsinclair@yahoo.com>
2024-11-25 22:45:28 -0500

    new bio added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,19 +20,26 @@ const iconarrow =  <LuArrowDownSquare />;
          <h3></h3>
         
 <div className='aboutme'>About me <LuArrowDownSquare /></div>
-         <p className='portraitBio'> I have been an artist my entire life. I was driven to teach myself to play piano at an early age 
-            and become a professional pianist. This was a true gift. I’ve painted, written songs, and have made some of the best pizza around the world. 
-
- 
-            In 2015 I was very lucky to have been at the right place at the right time and captured Syracuse’s Christmas tree. 
-            The photo went viral around the world and took third place in the prettiest Christmas tree internationally of that year.</p>
+         <p className='portraitBio'> I’ve been an artist my entire life. A creator. 
+          I was driven to teach myself to play piano at an early age and eventually become a professional pianist. 
+          I’ve written songs, painted, and I’ve even applied my creative passion as a pizzaiolo, crafting some of 
+          the best pizzas from around the world. </p>
             <p className='portraitBio'>
-            I believe, that in life, creating something is the path to happiness. Many years ago I suffered from an injury to my hands.  
-            I was unable to make music or do anything creative. I had a 8 megapixel point and shoot camera, I took that with me 
-            on my hikes, and found taking photos could be an excellent outlet for my artistic world. 
-           
-          I dove deep into learning everything I could about cameras and composition, I’m still learning. 
-          After years of shooting photographs I found a deep understanding that it’s not only satisfying but hard work.</p>
+            I believe that in life, creating something, anything, is the path to happiness. 
+            A few years ago, I suffered an injury to my hands,and suddenly I didn’t have the ability to play piano. 
+            But what Idid have was an eight-megapixel, point-and-shoot camera. 
+            I took it with me on my hikes and soon found that taking photos, 
+            capturing moments in time, was a great outlet for my artistic passion. 
+            I dove deep into learning everything I could about cameras and photography—technically and artistically. 
+            Fromlenses and formats to theme, composition, and color theory. 
+            </p>
+            <p className='portraitBio'>In 2015, I happened to be in the right place at the right time, 
+              capturing Syracuse’s beloved Christmas tree at a truly magical moment. 
+              The photo went viral world-wide and took third place in an international 
+              Prettiest Christmas Tree photography competition that year.
+            </p>
+            <p className='portraitBio'>To this day, I’m still learning, which I feel is key for any trueartist. And after years of shooting photographs, I’ve come tounderstand that while it’s hard work, it’s also very rewarding.
+            </p>
    
         
             </section>
