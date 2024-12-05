80d9af559cb073cca47264b8edffd1c7d9ea86f6
lucidbrawler <76524391+lucidbrawler@users.noreply.github.com>
2024-12-05 18:18:19 -0500

    spaced words out

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,18 +27,18 @@ const iconarrow =  <LuArrowDownSquare />;
             <p className='portraitBio'>
             I believe that in life, creating something, anything, is the path to happiness. 
             A few years ago, I suffered an injury to my hands,and suddenly I didn’t have the ability to play piano. 
-            But what Idid have was an eight-megapixel, point-and-shoot camera. 
+            But what I did have was an eight-megapixel, point-and-shoot camera. 
             I took it with me on my hikes and soon found that taking photos, 
             capturing moments in time, was a great outlet for my artistic passion. 
             I dove deep into learning everything I could about cameras and photography—technically and artistically. 
-            Fromlenses and formats to theme, composition, and color theory. 
+            From lenses and formats to theme, composition, and color theory. 
             </p>
             <p className='portraitBio'>In 2015, I happened to be in the right place at the right time, 
               capturing Syracuse’s beloved Christmas tree at a truly magical moment. 
               The photo went viral world-wide and took third place in an international 
               Prettiest Christmas Tree photography competition that year.
             </p>
-            <p className='portraitBio'>To this day, I’m still learning, which I feel is key for any trueartist. And after years of shooting photographs, I’ve come tounderstand that while it’s hard work, it’s also very rewarding.
+            <p className='portraitBio'>To this day, I’m still learning, which I feel is key for any trueartist. And after years of shooting photographs, I’ve come to understand that while it’s hard work, it’s also very rewarding.
             </p>
    
         
